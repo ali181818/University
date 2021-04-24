51b5db2b0639e69f7e1a7cdca376c3f87dc28c2f
MasterAli <alijan2056@gmail.com>
2021-04-25 02:56:18 +0430

    First Commit

---
+++ 2- Slider/Scripts/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const img = document.querySelector('header img');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+
+const imgList = ['Apple','Berry','Peach'];
+
+let i = 0;
+
+function animImg() {
+  img.classList.remove('slider-anim');
+  img.classList.add('slider-anim-rev');
+  setTimeout(() => {
+    img.classList.remove('slider-anim-rev');
+    img.classList.add('slider-anim');
+  }, 200);
+}
+
+setInterval(() => {
+  i = i == imgList.length - 1 ? 0 : i + 1;
+  animImg();
+  img.src = `./img/${imgList[i]}.jpg`;
+}, 5000);
+
+next.addEventListener('click',() => {
+  i = i == imgList.length - 1 ? 0 : i + 1;
+  animImg();
+  img.src = `./img/${imgList[i]}.jpg`;
+});
+
+prev.addEventListener('click',() => {
+  i = i == 0 ? 2 : i - 1;
+  animImg();
+  img.src = `./img/${imgList[i]}.jpg`;
+});
